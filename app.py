ee8ad97327760f85bd1b2a5e7477c2f9fcc38a17
GAUTAM YADAV <50361898+iamgautamy@users.noreply.github.com>
2024-05-06 02:04:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -96,4 +96,4 @@ def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
